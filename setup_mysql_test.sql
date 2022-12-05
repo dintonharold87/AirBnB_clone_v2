668ce848a2b6a2fa7dd0121edecfa1174f69daf3
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-12-05 17:27:34 +0300

    Updated setup_mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,19 @@
--- prepares a MySQL server for the project
--- A database hbnb_test_db and a new user hbnb_test (in localhost)
+-- Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+--   Grants all privileges for hbnb_test on hbnb_test_db.
+--   Grants SELECT privilege for hbnb_test on performance_schema.
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test@localhost';
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
