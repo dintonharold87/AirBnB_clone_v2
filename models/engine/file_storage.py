3e6ab2f4b581f77d1627a494791cad270eb4fff8
DonAine <30406256+DonAine@users.noreply.github.com>
2022-12-06 04:22:02 -0800

    File storage modifications 0.1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,10 +56,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-    
+
     def delete(self, obj=None):
         """Delete a given object from __objects, if it exists."""
         try:
