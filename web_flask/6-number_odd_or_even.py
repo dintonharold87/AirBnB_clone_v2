c9fb3e3bae7bdfd68cc8e1cc0ac23fe730420c6f
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-01-14 19:25:20 +0300

    Added task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -50,5 +50,14 @@ def number_template(n):
     if type(n) == int:
         return render_template('5-number.html', n=n)
 
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Returns a template at the /number_odd_or_even/<n>
+    route, display if the number is odd or even"""
+    if type(n) == int:
+        return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
